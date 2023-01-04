630e823c06dbd5a04c1399bd55f405e6906b7535
neighborstan <dropweb.post@gmail.com>
2023-01-04 23:10:31 +0600

    Added spring boot skeleton project.

---
+++ src/main/java/art/evalevi/telegrambot/statuscheckbot/CheckMyBiopassportKgApplication.java
@@ -0,0 +1,13 @@
+package art.evalevi.telegrambot.statuscheckbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckMyBiopassportKgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckMyBiopassportKgApplication.class, args);
+	}
+
+}
