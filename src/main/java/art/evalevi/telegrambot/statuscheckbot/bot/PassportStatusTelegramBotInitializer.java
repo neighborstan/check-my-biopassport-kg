8cebf9c2a94f95ac135699bc8e37ccfc8204dc7b
neighborstan <dropweb.post@gmail.com>
2023-01-06 14:00:05 +0600

    Added base code of telegram layer.

---
+++ src/main/java/art/evalevi/telegrambot/statuscheckbot/bot/PassportStatusTelegramBotInitializer.java
@@ -0,0 +1,32 @@
+package art.evalevi.telegrambot.statuscheckbot.bot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Slf4j
+@Component
+public class PassportStatusTelegramBotInitializer {
+
+    private final PassportStatusTelegramBot bot;
+
+    @Autowired
+    public PassportStatusTelegramBotInitializer(PassportStatusTelegramBot bot) {
+        this.bot = bot;
+    }
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(bot);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
+    }
+}
